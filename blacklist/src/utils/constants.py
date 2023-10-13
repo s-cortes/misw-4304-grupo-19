09794f2e9a1243281ce41677cf34f3a74944212c
coldblade2000 <diegogranada7@gmail.com>
2023-10-12 19:29:37 -0500

    [feat] added constants.py to centralize all environment variable loading

---
+++ blacklist/src/utils/constants.py
@@ -0,0 +1,8 @@
+import os
+
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
--- blacklist/src/utils/database.py
+++ blacklist/src/utils/database.py
@@ -1,14 +1,9 @@
 import os
 
+from utils.constants import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
+
 
 class DatabaseUtil:
     @staticmethod
     def generate_database_uri():
-        DB_USER = os.environ["DB_USER"]
-        DB_PASSWORD = os.environ["DB_PASSWORD"]
-
-        DB_HOST = os.environ["DB_HOST"]
-        DB_PORT = os.environ["DB_PORT"]
-        DB_NAME = os.environ["DB_NAME"]
-
         return f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
